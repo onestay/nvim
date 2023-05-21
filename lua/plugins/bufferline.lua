d4ce4478e4d62789458070772db9bb9a17db6ade
Marius Meschter <git@meschter.me>
2023-05-21 20:59:08 +0200

    feat(plugin): add mini.bufremove + bufferline keys

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,11 +2,37 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    {
+      "echasnovski/mini.bufremove",
+      -- stylua: ignore
+      keys = {
+        { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+        { "<leader>bD", function() require("mini.bufremove").delete(0, true) end,  desc = "Delete Buffer (Force)" },
+      },
+    },
+  },
+  event = "VeryLazy",
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
   },
   opts = {
     options = {
+      close_command = function(n)
+        require("mini.bufremove").delete(n, false)
+      end,
+      right_mouse_command = function(n)
+        require("mini.bufremove").delete(n, false)
+      end,
       diagnostics = "nvim_lsp",
       always_show_bufferline = false,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("config.icons").diagnostics
+        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+
+        return vim.trim(ret)
+      end,
       offsets = {
         {
           filetype = "neo-tree",
