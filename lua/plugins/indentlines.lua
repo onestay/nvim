0169a838b3e0cc21becc8d10165187288f3f3301
Marius Meschter <marius.meschter@univention.de>
2023-06-02 16:56:02 +0200

    remove animated indentlines

--- lua/plugins/indentlines.lua
+++ lua/plugins/indentlines.lua
@@ -5,28 +5,7 @@ return {
     opts = {
       show_current_context = false,
       show_trailing_blankline_indent = false,
-      char = "|",
       filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
     },
-  },
-  {
-    "echasnovski/mini.indentscope",
-    version = false,
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      symbol = "|",
-      options = { try_as_border = true },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-    config = function(_, opts)
-      require("mini.indentscope").setup(opts)
-    end,
   },
 }
