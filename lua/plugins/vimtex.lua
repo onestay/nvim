1d84462fcab166d4300ce6c8169e57b9ca0a141e
Marius Meschter <marius.meschter@univention.de>
2025-03-19 10:16:57 +0100

    disable vimtex syntax conceal

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,22 +4,7 @@ return {
     config = function()
       vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
       vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
-      
-      vim.g.vimtex_syntax_conceal = {
-        accents = 1,
-        ligatures = 1,
-        cites = 1,
-        fancy = 1,
-        spacing = 0,
-        greek = 1,
-        math_bounds = 1,
-        math_delimiters = 1,
-        math_fracs = 1,
-        math_super_sub = 1,
-        math_symbols = 1,
-        sections = 1,
-        styles = 1,
-      }
+      vim.g.vimtex_syntax_conceal_disable = true
     end,
   }
 }
